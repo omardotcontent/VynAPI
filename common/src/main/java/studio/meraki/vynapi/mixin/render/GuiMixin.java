4428421ad94b0cd637b33bccd738d5bfa17510e2
Omar Mohamed <omar@merakistudios.dev>
2026-08-07 18:30:59 +0300

    Updated GUI Again bec forge

---
+++ common/src/main/java/studio/meraki/vynapi/mixin/render/GuiMixin.java
@@ -0,0 +1,34 @@
+package studio.meraki.vynapi.mixin.render;
+
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import studio.meraki.vynapi.handler.other.DebugTextHandler;
+
+@Mixin(Gui.class)
+public abstract class GuiMixin {
+
+    @Shadow
+    public abstract Font getFont();
+
+    @Inject(
+            method = "render(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/DeltaTracker;)V",
+            at = @At("TAIL")
+    )
+    private void debugText(final GuiGraphics guiGraphics, final DeltaTracker deltaTracker, final CallbackInfo ci) {
+        if (DebugTextHandler.getRenderedTexts().isEmpty())
+            return;
+
+        int y = 10;
+        for (final String string : DebugTextHandler.getRenderedTexts()) {
+            guiGraphics.drawString(getFont(), string, 10, y, 0xFFFFFFFF, false);
+            y += 10;
+        }
+    }
+}
