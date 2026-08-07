4eec60082d3be45f10a8519b78fa972307838c00
Omar Mohamed <omar@merakistudios.dev>
2026-08-07 13:40:50 +0300

    Ported to the point that it just works

--- forge/src/main/java/studio/meraki/vynapi/VynAPI.java
+++ forge/src/main/java/studio/meraki/vynapi/VynAPI.java
@@ -1,9 +1,7 @@
 package studio.meraki.vynapi;
 
 import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import studio.meraki.vynapi.handler.script.ScriptLoader;
 import studio.meraki.vynapi.model.variable.ModLoader;
 import studio.meraki.vynapi.platform.Services;
@@ -11,14 +9,12 @@ import studio.meraki.vynapi.platform.Services;
 @Mod(Services.MOD_ID)
 public class VynAPI {
 
-    public VynAPI(final FMLJavaModLoadingContext context) {
-        final IEventBus eventBus = context.getModEventBus();
-
+    public VynAPI() {
         Services.LOG.info("Hello from VynAPI on Forge!");
         new ModLoader(new ForgeModLoaderHandler());
 
         ScriptLoader.init();
-        eventBus.addListener(this::registerReloadListeners);
+        RegisterClientReloadListenersEvent.BUS.addListener(this::registerReloadListeners);
     }
 
     private void registerReloadListeners(final RegisterClientReloadListenersEvent event) {
