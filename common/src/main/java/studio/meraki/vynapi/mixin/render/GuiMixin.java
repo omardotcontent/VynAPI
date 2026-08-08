513ac56adcdf66c970fe098838d2a2bcb4aaae5d
Omar Mohamed <omar@merakistudios.dev>
2026-08-08 12:29:57 +0300

    Gui Mixin Update for 26.2 Support

--- common/src/main/java/studio/meraki/vynapi/mixin/render/GuiMixin.java
+++ common/src/main/java/studio/meraki/vynapi/mixin/render/GuiMixin.java
@@ -1,10 +1,11 @@
 package studio.meraki.vynapi.mixin.render;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.gui.Font;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
-
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,20 +16,22 @@ import studio.meraki.vynapi.handler.other.DebugTextHandler;
 @Mixin(Gui.class)
 public abstract class GuiMixin {
 
+    @Final
     @Shadow
-    public abstract Font getFont();
+    private Minecraft minecraft;
 
     @Inject(
             method = "extractRenderState",
             at = @At("TAIL")
     )
-    private void debugText(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+    private void debugText(DeltaTracker deltaTracker, boolean shouldRenderLevel, boolean resourcesLoaded, CallbackInfo ci,
+                           @Local(name = "graphics") GuiGraphicsExtractor graphics) {
         if (DebugTextHandler.getRenderedTexts().isEmpty())
             return;
 
         int y = 10;
         for (final String string : DebugTextHandler.getRenderedTexts()) {
-            graphics.text(getFont(), string, 10, y, 0xFFFFFFFF, false);
+            graphics.text(this.minecraft.font, string, 10, y, 0xFFFFFFFF, false);
             y += 10;
         }
     }
